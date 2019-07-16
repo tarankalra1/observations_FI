b017f61217547ab2a410e52b80829dff7a9f3731
ssuttles-usgs <ssuttles@usgs.gov>
2019-07-16 12:43:14 -0400

    add waveform scripts for data

---
+++ findwfr.m
@@ -0,0 +1,29 @@
+function wfr=findwfr(wf)
+
+
+for i=1:length(wf)
+ubh(i)=wf(i).umax-wf(i).umin;
+end
+ubhr=mean(ubh)
+
+for i=1:length(wf)
+Tcr(i)=wf(i).Tc/ubhr;
+Ttr(i)=wf(i).Tt/ubhr;
+Acr(i)=wf(i).Ac/ubhr;
+Atr(i)=wf(i).At/ubhr;
+umaxr(i)=wf(i).umax/ubhr;
+uminr(i)=wf(i).umin/ubhr;
+tmax(i)=wf(i).tc_max/wf(i).Tc;
+tmin(i)=wf(i).tt_max/wf(i).Tt;
+end
+
+wfr.ubh=ubhr;
+wfr.umax=mean(umaxr)*ubhr;
+wfr.umin=mean(uminr)*ubhr;
+wfr.Tc=mean(Tcr)*ubhr;
+wfr.Tt=mean(Ttr)*ubhr;
+wfr.Ac=mean(Acr)*ubhr;
+wfr.At=mean(Atr)*ubhr;
+wfr.tmax=mean(tmax)*wfr.Tc;
+wfr.tmin=mean(tmin)*wfr.Tt;
+wfr.R=wfr.umax/(wfr.umax-wfr.umin);
